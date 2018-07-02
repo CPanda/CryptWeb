d0895cbc2a2bc63fb21823f040f92ffbb20cb524
CPanda <djh33454@huskies.bloomu.edu>
2018-07-01 20:11:18 -0400

    Analysis object

---
+++ Models/Analysis.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * This class contains some helper methods and analysis options for the cipher text. 
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CryptoWeb.Models
+{
+    public class Analysis
+    {
+        private string encryptedtext; 
+        
+        
+        /**
+         * returns a string with removed special characters and spaces for processing. 
+         */
+        public static string processtext(string text)
+        {
+            text = text.ToLower();
+            Regex rgx = new Regex("^a-zA-Z");
+            text = rgx.Replace(text, ""); 
+            Regex rgx2 = new Regex(@"r'\s+");
+            text = rgx2.Replace(text, "");
+            return text; 
+        }
+
+        /**
+         * Returns a dictionary containing the letter and the number of occurances of that letter.
+         * 
+         */
+        public static Dictionary<char, int> stringCounter(string text)
+        {
+            Dictionary<char, int> dict = text.Where(c => char.IsLetter(c)).GroupBy(c => c)
+                .ToDictionary(g => g.Key, g => g.Count());
+            return dict; 
+
+        }
+    }
+}
