f11f9fc7a85540ceab930222b6adedc7bc594b30
CPanda <djh33454@huskies.bloomu.edu>
2018-07-06 14:23:40 -0400

    Removing default template pages.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -58,17 +58,11 @@ namespace CryptoWeb.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Testing C#";
 
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
 
         public IActionResult Privacy()
         {
